69f2d4e7cfc05e7cc5f22f1872fc2aebee64dadf
Hritik kumar <Hritikkumar0015@gmail.com>
2024-12-22 12:28:26 +0530

    protect private variable & function

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // one module to another
-require('./sum')
+const obj=require('./sum')
+// const {x,caluculateSum}=require('./sum')
+
 
 var x="Hritik Kumar";
 console.log(x)
 
-// this won't work, Modules protects thier variable and function from leaking  --> calculateSum is not defined
-caluculateSum(5,3)
+// this works now
+obj.caluculateSum(5,3)
+
+console.log(obj.x)
--- sum.js
+++ sum.js
@@ -5,3 +5,17 @@ function caluculateSum(a,b){
     const sum=a+b;
     console.log(sum)
 }
+
+var x=100;
+
+// module.exports=caluculateSum;
+
+// module.exports={
+//     x:x,
+//     caluculateSum:caluculateSum,
+// }
+
+module.exports={
+    x,
+    caluculateSum,
+}
