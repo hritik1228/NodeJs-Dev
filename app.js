898a0d3befdd30dacb1f92ee8a29ab3f338e22b0
Hritik kumar <Hritikkumar0015@gmail.com>
2024-12-22 11:59:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// one module to another
+require('./sum')
+
+var x="Hritik Kumar";
+console.log(x)
+
+// this won't work --> calculateSum is not defined
+caluculateSum(5,3)
---
+++ sum.js
@@ -0,0 +1,7 @@
+// console.group(5+6)
+
+
+function caluculateSum(a,b){
+    const sum=a+b;
+    console.log(sum)
+}
