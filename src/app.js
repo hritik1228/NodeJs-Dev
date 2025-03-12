5890c7840596a1bb61477de617d48a0892b64745
Hritik kumar <Hritikkumar0015@gmail.com>
2025-03-12 22:01:20 +0530

    Refactor database connection logic to ensure server starts only after successful connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express=require("express");
-require("../src/config/database")
+const connectDB=require("./config/database")
 
 const app=express()
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+// First of all connect to database & then listen to the port/incoming request
+// Once your database connection is successfully established then only do app.listen
+
+connectDB().then(()=>{   
+    console.log("Connected to Cluster")
+    // Once it is connected to database here we should idealy call app.listen 
+    app.listen(3000,()=>{
+        console.log("Server is running on port 3000")
+    })
+}).catch((err)=>{
+    console.log(err)
 })
+
--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,11 @@ const connectDB=async()=>{
     await mongoose.connect("mongodb+srv://hritiklearntogrow:GvKSNIy9IqU5PR3J@node-learn.sgrhf.mongodb.net/")
 }
 
+module.exports=connectDB
 
-connectDB().then(()=>{   
-    console.log("Connected to Cluster")
-}).catch((err)=>{
-    console.log(err)
-})
+// connectDB().then(()=>{   
+//     console.log("Connected to Cluster")
+//     // Once it is connected to database here we should idealy call app.listen 
+// }).catch((err)=>{
+//     console.log(err)
+// })
