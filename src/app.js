07c345ad02a8c3e561661d0cd2721d9aa0766dc5
Hritik kumar <Hritikkumar0015@gmail.com>
2025-03-15 11:09:24 +0530

    Add JSON parsing middleware and update signup route to use dynamic data

--- src/app.js
+++ src/app.js
@@ -3,18 +3,28 @@ const connectDB=require("./config/database")
 const User=require("./models/user")
 const app=express()
 
+// this middleware is used to parse the incoming request with JSON payloads 
+// It is used for all the routes
+
+// When a client (e.g., a frontend app) sends data to your Express server using POST or PUT, the data is usually in JSON format. However, Express does not automatically understand JSON. express.json() helps Express convert the incoming JSON data into a JavaScript object so you can easily use it in your code.
+app.use(express.json())
+
 app.post("/signup",async(req,res)=>{
 
-    const userObj={
-        firstName:"Khushi",
-        lastName:"Kumari",
-        emailId:"khushi@kumari.gmail.com",
-        password:"khushi@123"
-    }
+    
+    // console.log(req.body)
+
+    // const userObj={
+    //     firstName:"Khushi",
+    //     lastName:"Kumari",
+    //     emailId:"khushi@kumari.gmail.com",
+    //     password:"khushi@123"
+    // }
 
     try{
         // Creating a new instance of the user model
-        const user = new User(userObj);
+        // const user = new User(userObj);
+        const user = new User(req.body);
         await user.save();
         res.send("User added successfully");
     }catch(err){
@@ -36,3 +46,18 @@ connectDB().then(()=>{
     console.log(err)
 })
 
+
+
+// Write a basic difference between JSON & Javascript object?
+// JSON is a string format used to store and exchange data.
+// JavaScript object is a real object used in JavaScript code
+
+// const jsonData = '{"name": "John", "age": 30}';
+// const obj = JSON.parse(jsonData); // Converts JSON string to object
+
+// const obj = { name: "John", age: 30 };
+// const jsonData = JSON.stringify(obj); // Converts object to JSON string
+
+// Usage
+// JSON-> Used for data exchange (e.g., API responses)	
+// JS Object-> Used for programming logic in JavaScript
