379be54a74cc4816e68749c88c32826312874c2e
Hritik kumar <Hritikkumar0015@gmail.com>
2025-03-16 09:22:11 +0530

    Enhance user model with data validation and sanitization for fields

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,52 @@
 const mongoose=require("mongoose");
 
+// Data Sanitization & Schema Validation
+
 const userSchema=new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minlength:4,
+        maxlength:50,
     },
     lastName:{
         type:String,
     },
-    email:{
+    emailId:{
         type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
         type:Number,
+        min:18,
     },
     gender:{
-        type:String,    
+        type:String,   
+        // It will only be called when the data is inserted on updation it won't be called
+        validate(value){
+            if(!["male","female","others"].includes(value)){    
+                throw new Error("Gender data is not valid");
+            }
+        }, 
+    },
+    photoUrl:{
+        type:String,
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user",
+    },
+    skills:{
+        type:[String],
     }
+
 })
 
 // User is the model name-> it should be in capital letter
