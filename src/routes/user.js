1415177c286836c60a4fe1ac4c7f183c56f8e71d
Hritik kumar <Hritikkumar0015@gmail.com>
2026-04-04 10:22:20 +0530

    user feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -61,4 +61,39 @@ userRouter.get("/user/connections",
 
     })
 
+ userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page || 1);
+    let limit = parseInt(req.query.limit || 10);
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+});
+
 module.exports = userRouter;
